d8e925be7e6c00bb558e98df996eb54bd3ea7d0e
timescalexr <seraydogdu@gmail.com>
2021-01-11 13:09:05 +0300

    first commit

---
+++ PPMToolFullStack/src/test/java/com/agileintelligence/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agileintelligence.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
